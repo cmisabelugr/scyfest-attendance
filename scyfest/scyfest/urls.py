73dac2cf22c01ff7b051b1d61753b811aef3cfa3
Javier Saez <jscoba@gmail.com>
2022-04-18 17:45:41 +0200

    Init frontend

--- scyfest/scyfest/urls.py
+++ scyfest/scyfest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from attendance.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test_view, name="test_view")
 ]
